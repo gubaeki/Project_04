f8f192e18c708bbbeb373cf2c3586ecac5dd11d2
gubaeki <110735099+gubaeki@users.noreply.github.com>
2024-09-29 19:18:05 +0900

    Add files via upload

--- assets/js/room1.js
+++ assets/js/room1.js
@@ -3,13 +3,25 @@ var room = document.getElementById('room_container');
 var roomRect = room.getBoundingClientRect();
 var roomWidth = roomRect.width;
 var roomHeight = roomRect.height;
+var bottom_img = document.getElementById('bottom_img');
 var return_img = document.getElementById('return_img');
 var hammer_bg = document.getElementById('hammer_bg');
 var memo_bg_red = document.getElementById('memo_bg_red');
+var memo_bg_blue = document.getElementById('memo_bg_blue');
 var memo_red = document.getElementById('memo_red');
+var memo_blue = document.getElementById('memo_blue');
 var towel_min = document.getElementById('towel_min');
 var switch_Off = document.getElementById('switch_Off');
 var dark2 = document.getElementById('dark2');
+var tile = document.getElementById('tile');
+var light = document.getElementById('light');
+var safe = document.getElementById('safe');
+var safe_number = document.getElementById('safe_number');
+var key = document.getElementById('key');
+var glass = document.getElementById('glass');
+var success = document.getElementById('success');
+var gotomain = document.getElementById('gotomain');
+
 
 //item 관련선언
 var item = [,,,,];
@@ -18,11 +30,9 @@ item[2] = document.getElementById('item_2');
 item[3] = document.getElementById('item_3');
 item[4] = document.getElementById('item_4');
 
-var ItemFillNum = 0; //채워진 아이템 번호
-var itemFillName = [,,,,]; //채워진 아이템 이름
+var itemFillName = ['base','none','none','none','none']; //채워진 아이템 이름
 var itemSelectNum = 0; //선택한 아이템 번호
 var itemSelectName; //선택한 아이템 이름
-
 
 
 //기타 변수
@@ -33,13 +43,22 @@ var switch_open = true;
 var darkness = false;
 var memo_open = false;
 var wall_broken = false;
+var tile_broken = false;
+var safe_value;
+var safe_value_next;
+var safe_count = 0;
+var safe_open = false;
+var subroom_open = false;
+var subroom_in = false;
+var glass_get = false;
+var backho_get = false;
 
 document.addEventListener('click', function(event) {
 
-    /*
-    if (message.style.display === 'block') {
+    
+    if (backho_get) {
         return; // 메시지가 이미 표시되고 있을 때는 클릭 이벤트를 무시
-    }*/
+    }
 
     //터치좌표 입력 시 clientX 대신 pageX 사용(clientX는 현재보이는 화면 좌측 상단이 무조건 0, pageX는 문서의 좌측 상단이 0이고 스크롤이 생겨도 화면 좌측상단은 0이 아님)
     var x = event.pageX;
@@ -65,6 +84,20 @@ document.addEventListener('click', function(event) {
             if(hammer_get == false){
                 hammer_bg.style.display = 'block';
             }
+            if(tile_broken){
+                tile.style.display = 'block';
+                light.style.display = 'none';
+            }
+            else{
+                if(darkness){
+                    light.style.display = 'block';
+                    tile.style.display = 'none';
+                }
+                else{
+                    light.style.display = 'none';
+                    tile.style.display = 'none';
+                }
+            }
             
         }
     
@@ -83,7 +116,12 @@ document.addEventListener('click', function(event) {
 
         if(x < roomWidth * 0.83 && x > roomWidth * 0.7 && y < roomHeight * 0.4){ // 우측상단 2사로 클릭
             console.log("2사로");
-            room.setAttribute('src', 'images/room1-3.png');
+            if(subroom_open){
+                room.setAttribute('src', 'images/room1-3-1.png');
+            }
+            else{
+                room.setAttribute('src', 'images/room1-3.png');
+            }
             return_img.style.display = 'block';
             room_number = 4;
         }
@@ -106,8 +144,27 @@ document.addEventListener('click', function(event) {
         if(x < roomWidth * 0.2 && y > roomHeight * 0.83 && y < roomHeight * 0.97){ // 메인룸 돌아가기
             room.setAttribute('src', 'images/room1.png');
             return_img.style.display = 'none';
+            memo_bg_blue.style.display = 'none';
             hammer_bg.style.display = 'none';
+            tile.style.display = 'none';
+            light.style.display = 'none';
             room_number = 1;
+        }
+        if(x < roomWidth * 0.6 && x > roomWidth * 0.5 && y > roomHeight * 0.62 && y < roomHeight * 0.72){ // 빛 위치 클릭
+            if(darkness && tile_broken == false){
+                light.style.display = 'none';
+                tile.style.display = 'block';
+                tile_broken = true;
+                memo_bg_blue.style.display = 'block';
+                var a = 1;
+                    var interval = setInterval(function(){
+                        memo_bg_blue.style.opacity = a;
+                        a = a - 0.1;
+                        if(a < 0){clearInterval(interval);};
+                    }, 100);
+                item_get('memo_blue');
+            }
+            console.log('1');
         }
 
     }
@@ -116,29 +173,25 @@ document.addEventListener('click', function(event) {
         if(x < roomWidth * 0.2 && y > roomHeight * 0.8){ // 메인룸 돌아가기
             room.setAttribute('src', 'images/room1.png');
             return_img.style.display = 'none';
+            memo_bg_red.style.display = 'none';
             room_number = 1;
         }
         if(x < roomWidth * 0.78 && x > roomWidth * 0.67 && y > roomHeight * 0.35 && y < roomHeight * 0.45){ // 균열 클릭
-            if(itemSelectNum != 0){
-                if(itemSelectName === 'hammer'){
-                    room.setAttribute('src', 'images/room1-2-1.png');
-                    memo_bg_red.style.display = 'block';
-                    wall_broken = true;
-                    var a = 1;
-                    var interval = setInterval(function(){
-                        memo_bg_red.style.opacity = a;
-                        a = a - 0.1;
-                        if(a < 0){clearInterval(interval);};
-                    }, 100);
-                    itemFillName[itemSelectNum] = "memo_red";
-                    item[itemSelectNum].setAttribute('src', 'images/memo_bg_red.png');
-                    itemSelectNum = 0;
-                    itemSelectName = 0;
-                    item[1].style.backgroundColor="transparent";
-                    item[2].style.backgroundColor="transparent";
-                    item[3].style.backgroundColor="transparent";
-                    item[4].style.backgroundColor="transparent";
-                }
+            if(itemSelectName === 'hammer'){
+                room.setAttribute('src', 'images/room1-2-1.png');
+                memo_bg_red.style.display = 'block';
+                wall_broken = true;
+                var a = 1;
+                var interval = setInterval(function(){
+                    memo_bg_red.style.opacity = a;
+                    a = a - 0.1;
+                    if(a < 0){clearInterval(interval);};
+                }, 100);
+
+                item_used('hammer');
+                item_reset();
+                item_get('memo_red');
+
             }
         }
     }
@@ -147,8 +200,49 @@ document.addEventListener('click', function(event) {
         if(x < roomWidth * 0.2 && y > roomHeight * 0.8){ // 메인룸 돌아가기
             room.setAttribute('src', 'images/room1.png');
             return_img.style.display = 'none';
+            subroom_in = false;
             room_number = 1;
         }
+        if(subroom_open){
+            if(subroom_in){
+                if(x > roomWidth * 0.25 && x < roomWidth * 0.75 && y > roomHeight * 0.3 && y < roomHeight * 0.7){ // 백호돌이 클릭
+                    if(itemSelectName == 'glass'){
+                        room.setAttribute('src', 'images/room1-3-3.png');
+                        backho_get = true;
+                        success.style.display = 'block';
+                        bottom_img.setAttribute('src', 'images/bottom_success.png');
+                        dark2.style.display = 'none';
+                        return_img.style.display = 'none'
+                        gotomain.style.display='block';
+                        item_used('glass');
+                        item_reset();
+                    }
+                }
+            }
+            else{
+                if(y < roomHeight * 0.7){ // 백호돌이방 들어가기
+                    room.setAttribute('src', 'images/room1-3-2.png');
+                    subroom_in = true;
+                }
+            }
+            
+        }
+        else{
+            if(x > roomWidth * 0.17 && x < roomWidth * 0.43 && y > roomHeight * 0.3 && y < roomHeight * 0.61){ // 열쇠로 자물쇠 오픈
+                if(itemSelectName === 'key'){
+                    subroom_open = true;
+                    room.setAttribute('src', 'images/room1-3-1.png');
+    
+                    item_used('key');
+                    item_reset();
+    
+                }
+            }
+        }
+        
+        
+        
+
     }
 
     else if(room_number === 5){ // 좌측면(정문)
@@ -157,6 +251,9 @@ document.addEventListener('click', function(event) {
             return_img.style.display = 'none';
             towel_min.style.display = 'none';
             switch_Off.style.display = 'none';
+            safe.style.display = 'none';
+            safe_number.style.display = 'none';
+            safe_reset();
             room_number = 1;
 
         }
@@ -166,8 +263,8 @@ document.addEventListener('click', function(event) {
                 towel_open = true;
             }
             else{
-                towel_min.style.display = 'none';
-                towel_open = false;
+                safe.style.display = 'block';
+                safe_number.style.display = 'block';
             }
         }
         if(x > roomWidth * 0.9 && x < roomWidth * 0.95 && y > roomHeight * 0.55 && y < roomHeight * 0.66){ // 스위치 클릭
@@ -184,8 +281,24 @@ document.addEventListener('click', function(event) {
                 darkness = false;
             }
         }
-    }
+        if(x > roomWidth * 0.7 && x < roomWidth * 0.8 && y > roomHeight * 0.06 && y < roomHeight * 0.17){ // 시계 클릭
+            if(glass_get == false){
+                item_get('glass');
+                glass_get = true;
+            }
+            else{
+                //skip
+            }
+   
+        }
 
+
+        if(y < roomHeight * 0.2 || (y < roomHeight * 0.95 && y > roomHeight * 0.8)){  // 금고 창 닫기
+            safe.style.display = 'none';
+            safe_number.style.display = 'none';
+            safe_reset();
+        }
+    }
 });
 
 // 해머 발견
@@ -199,10 +312,12 @@ hammer_bg.addEventListener('click', function(event) {
 memo_red.addEventListener('click', function(event) {
     memo_red.style.display = 'none';
     memo_open = false;
-    item[1].style.backgroundColor="transparent";
-    item[2].style.backgroundColor="transparent";
-    item[3].style.backgroundColor="transparent";
-    item[4].style.backgroundColor="transparent";
+    item_reset();
+});
+memo_blue.addEventListener('click', function(event) {
+    memo_blue.style.display = 'none';
+    memo_open = false;
+    item_reset();
 });
 
 // 아이템컨테이너에서 아이템 선택 시
@@ -272,16 +387,63 @@ item[4].addEventListener('click', function(event) {
 });
 
 function item_get(itemName){
-    ItemFillNum++;
-    itemFillName[ItemFillNum] = itemName;
-    if(itemName=='hammer'){
-        item[ItemFillNum].setAttribute('src', 'images/hammer_item.png');
-    }
+    console.log('item get');
+    var i;
+    for(i=1;i<5;i++){
+        if(itemFillName[i]=='none'){
+            itemFillName[i] = itemName;
+            if(itemName=='hammer'){
+                item[i].setAttribute('src', 'images/hammer_item.png');
+            }
+            else if(itemName=='memo_red'){
+                item[i].setAttribute('src', 'images/memo_bg_red.png');
+            }
+            else if(itemName=='memo_blue'){
+                item[i].setAttribute('src', 'images/memo_bg_blue.png');
+            }
+            else if(itemName=='key'){
+                item[i].setAttribute('src', 'images/key.png');
+            }
+            else if(itemName=='glass'){
+                item[i].setAttribute('src', 'images/glass.png');
+            }
+
+            break;
+        }
+        else{
+            //skip
+        }
+    } 
+}
+function item_used(itemName){
+    console.log('item used');
+    var i;
+    for(i=1;i<5;i++){
+        if(itemFillName[i]==itemName){
+            itemFillName[i] = 'none';
+            item[i].setAttribute('src', 'images/sample.png');
+            break;
+        }
+        else{
+            //skip
+        }
+    } 
+}
+function item_reset(){
+    console.log('item reset');
+    itemSelectNum = 0;
+    itemSelectName = 0;
+    item[1].style.backgroundColor="transparent";
+    item[2].style.backgroundColor="transparent";
+    item[3].style.backgroundColor="transparent";
+    item[4].style.backgroundColor="transparent";
 }
 
-function memo_check(itemSelectName){ //메모가 열려있는지 확인, 파랑 추가 필요
+
+function memo_check(itemSelectName){
     if(memo_open){
         memo_red.style.display = 'none';
+        memo_blue.style.display = 'none';
         memo_open = false;
         item[1].style.backgroundColor="transparent";
         item[2].style.backgroundColor="transparent";
@@ -291,7 +453,108 @@ function memo_check(itemSelectName){ //메모가 열려있는지 확인, 파랑 
     else{
         if(itemSelectName=='memo_red'){
             memo_red.style.display = 'block';
+            memo_blue.style.display = 'none';
+            memo_open = true;
+        }
+        else if(itemSelectName=='memo_blue'){
+            memo_blue.style.display = 'block';
+            memo_red.style.display = 'none';
             memo_open = true;
         }
     }
 }
+
+// 금고 비밀번호 입력
+safe.addEventListener('click', function(event) {
+    var x = event.pageX;
+    var y = event.pageY;
+
+    if(safe_open == false){
+        if(x > roomWidth * 0.675 && x < roomWidth * 0.745 && y > roomHeight * 0.585 && y < roomHeight * 0.635){ // 0
+            safe_check('0');
+        }
+        else if(x > roomWidth * 0.585 && x < roomWidth * 0.655 && y > roomHeight * 0.515 && y < roomHeight * 0.565){ // 1
+            safe_check('1');
+        }
+        else if(x > roomWidth * 0.675 && x < roomWidth * 0.745 && y > roomHeight * 0.515 && y < roomHeight * 0.565){ // 2
+            safe_check('2');
+        }
+        else if(x > roomWidth * 0.765 && x < roomWidth * 0.835 && y > roomHeight * 0.515 && y < roomHeight * 0.565){ // 3
+            safe_check('3');
+        }
+        else if(x > roomWidth * 0.585 && x < roomWidth * 0.655 && y > roomHeight * 0.445 && y < roomHeight * 0.495){ // 4
+            safe_check('4');
+        }
+        else if(x > roomWidth * 0.675 && x < roomWidth * 0.745 && y > roomHeight * 0.445 && y < roomHeight * 0.495){ // 5
+            safe_check('5');
+        }
+        else if(x > roomWidth * 0.765 && x < roomWidth * 0.835 && y > roomHeight * 0.445 && y < roomHeight * 0.495){ // 6
+            safe_check('6');
+        }
+        else if(x > roomWidth * 0.585 && x < roomWidth * 0.655 && y > roomHeight * 0.385 && y < roomHeight * 0.425){ // 7
+            safe_check('7');
+        }
+        else if(x > roomWidth * 0.675 && x < roomWidth * 0.745 && y > roomHeight * 0.385 && y < roomHeight * 0.425){ // 8
+            safe_check('8');
+        }
+        else if(x > roomWidth * 0.765 && x < roomWidth * 0.835 && y > roomHeight * 0.385 && y < roomHeight * 0.425){ // 9
+            safe_check('9');
+        }
+        else if(x > roomWidth * 0.765 && x < roomWidth * 0.835 && y > roomHeight * 0.585 && y < roomHeight * 0.635){ // Del
+            safe_reset();
+        }
+    }
+    
+    event.preventDefault();
+    
+});
+
+//금고 비밀번호 체크 함수
+function safe_check(safe_value_next){
+    if(safe_count === 0){
+        safe_value = safe_value_next;
+        safe_count++;
+        console.log(safe_value);
+    }
+    else{
+        if(safe_count === 4){
+            safe_value = safe_value_next;
+            safe_count = 1;
+        }
+        else{
+            safe_value = safe_value + safe_value_next;
+            safe_count++;
+        }
+        console.log(safe_value);
+    }
+    safe_number.textContent = safe_value;
+    if(safe_value=='1942'){
+        safe_open = true;
+        safe_number.style.display = 'none';
+        safe.setAttribute('src', 'images/safe_open.png');
+        key.style.display = 'block';
+        var a = 1;
+        var interval = setInterval(function(){
+            key.style.opacity = a;
+            a = a - 0.1;
+            if(a < 0){clearInterval(interval);};
+        }, 100);
+
+        item_used('memo_red');
+        item_used('memo_blue');
+        item_reset();
+        item_get('key');
+        
+    }
+} 
+function safe_reset(){
+    safe_count = 0;
+    safe_value = 0;
+    safe_number.textContent = '';
+}
+
+gotomain.addEventListener('click', function(event) {
+    gotomain.style.display = 'none';
+    location.replace('https://gubaeki.github.io/Project_04');
+
+});
