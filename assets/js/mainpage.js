1bf7647690885d618f15cfa1f55b4cf0640088bf
gubaeki <110735099+gubaeki@users.noreply.github.com>
2024-10-25 22:55:14 +0900

    Add files via upload

---
+++ assets/js/mainpage.js
@@ -0,0 +1,57 @@
+var room = document.getElementById('intro_container');
+var roomRect = room.getBoundingClientRect();
+var roomWidth = roomRect.width;
+var roomHeight = roomRect.height;
+const intro_container = document.getElementById('intro_container')
+var loading = document.getElementById('loading');
+
+
+
+
+//---------------------------------------------------------------
+// 이미지 Preload
+let images = [
+    "../../images/main.png",
+];
+    let images_pre = [];
+
+    var fail_count = 0;
+    
+    function preload(images) {
+        for(let i = 0; i < images.length; i++) {
+            images_pre[i] = new Image();
+            images_pre[i].src = images[i];
+            if(images_pre[i].complete == false){
+                fail_count++;
+            }
+            
+          if(i === images.length - 1){console.log('preload finish');}
+        }
+      }
+    
+    preload(images);
+    console.log(fail_count);
+    
+    
+    window.onload = function(){
+        console.log('로드완료');
+        loading.style.display = 'none';
+    }
+
+
+
+document.addEventListener('click', function(event) {
+
+    var x = event.pageX;
+    var y = event.pageY;
+
+    if(x > roomWidth * 0.2 && x < roomWidth * 0.8 && y < roomHeight * 0.45 && y > roomHeight * 0.08){ // 터치패드 클릭
+        console.log('방1');
+        location.href='room_first.html'
+    }
+    else if(x > roomWidth * 0.2 && x < roomWidth * 0.8 && y < roomHeight * 0.95 && y > roomHeight * 0.55){ // 터치패드 클릭
+        console.log('방2');
+        location.href='room_second.html'
+    }
+    
+});
